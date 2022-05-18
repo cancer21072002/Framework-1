248cfaa0ad7c6037d9995feaec3b9e3c4f4d3c72
cancer21072002 <92455145+cancer21072002@users.noreply.github.com>
2022-05-18 07:16:44 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,12 +28,56 @@ export class AppComponent {
 
   champs = [
     {
-      name: 'Lucian',
+      name: 'Ahri',
       dame: 400,
       defend: 200,
       speed: 100,
       price: 6300,
-      avatar: 'https://oneesports.blob.core.windows.net/cdn-data/sites/4/2021/08/pulsefire-lucian-lien-minh-huyen-thoai1.jpg'
+      avatar: 'https://kenhsongao.com/uploads/2021/05/photo-1-16154710895551258162210.jpg'
     }
-  ]
+  ];
+   studentName = 'vandth';
+  studentId = 'PH13979';
+
+  // Sự kiện
+  // Biến lưu trạng thái hiển thị bảng
+  showStatus = true;
+  onClickBtn() {
+    console.log("Btn clicked!");
+    this.showStatus = !this.showStatus;
+  }
+  inputValue={
+    name:'',
+    avatar:'',
+    dame:'',
+    price:''
+  }
+  onInput(event:any, key:'name'|'avatar'|'dame'|'price'){
+    this.inputValue[key]=event.target.value;
+  }
+
+  // inputName='';
+  // onInputName(event:any){
+  //   this.inputName= event.target.value;
+  // }
+  // inputAvatar='';
+  // onInputAvatar(event:any){
+  //   this.inputAvatar=event.target.value;
+  // }
+  onSubmit(){
+    console.log('Gia tri cac o Input',this.inputValue);
+  this.champs.push({}...this.inputValue,
+    dame:+this.inputValue.dame,
+    price:+this.inputValue.price
+    )
+  };
+  this.inputValue={
+    name:'',
+    avatar:'',
+    dame:'',
+    price:'',
+
+  };
 }
+
+
--- src/app/identity/identity.component.ts
+++ src/app/identity/identity.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-identity',
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./identity.component.css']
 })
 export class IdentityComponent implements OnInit {
-
-  constructor() { }
+  @Input() id:String;
+  constructor() {
+    this.id ='';
+   }
 
   ngOnInit(): void {
   }
